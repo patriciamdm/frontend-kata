b24f3f98411cf7ac3455df9ee724836d531c7c3b
Patricia <patrimdm96@gmail.com>
2021-02-18 13:29:31 +0100

    first commit

---
+++ kata/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+import MainBlock from './Main-block'
+
+function App() {
+  return (
+    <main>
+      <h1>hello!</h1>
+      <MainBlock />
+    </main>
+  );
+}
+
+export default App;
---
+++ kata/src/components/Main-block.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const MainBlock = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h4>Passenger info</h4>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default MainBlock;
